cf18096ce221257c6ce7cb4e35a8eafe8e68c63b
Mathias Blanc <mathias.blanc@eca-vaud.ch>
2019-08-04 17:31:52 +0200

    added error handling and input timer

--- pico.c
+++ pico.c
@@ -3,15 +3,23 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 struct termios orig_termios;
+
+void die(char *message)
+{
+    perror(message);
+    exit(1);
+}
 
 /* 
 * reset terminal settings to their original values
 */
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 /* 
@@ -22,7 +30,9 @@ void disableRawMode()
 */
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
+
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -30,26 +40,32 @@ void enableRawMode()
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcgetattr");
 }
 
 int main()
 {
     enableRawMode();
 
-    char c;
-
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
+        char c = '\0';
+
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+
         if (iscntrl(c))
-        {
             printf("%d\r\n", c);
-        }
         else
-        {
             printf("%d ('%c')\r\n", c, c);
-        }
-    };
+
+        if (c == 'q')
+            break;
+    }
 
     return 0;
 }
