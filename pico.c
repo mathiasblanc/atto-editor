b650a21b4290f18c6d374061f9026d98d05073ae
Mathias Blanc <mathias.blanc@eca-vaud.ch>
2019-08-04 15:57:54 +0200

    added raw mode

--- pico.c
+++ pico.c
@@ -1,12 +1,35 @@
 #include <unistd.h>
 #include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
+struct termios orig_termios;
+
+/* 
+* reset terminal settings to their original values
+*/
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+/* 
+* put terminal into raw mode : 
+* disable canonical mode -> input is read byte by byte instead of line by line
+* disable ctrl+* keys
+* disable OPOST (output processing) -> now requires explicit \r before \n
+*/
 void enableRawMode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-    raw.c_lflag &= ~(ECHO);
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -15,8 +38,18 @@ int main()
     enableRawMode();
 
     char c;
+
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-        ;
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    };
 
     return 0;
 }
